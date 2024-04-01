f0a16fe5c1c0c2ef3db6c393a60e92070f1f15b7
User <tal.feldman22@gmail.com>
2024-04-01 14:09:25 +0300

    need to finish game

--- ViewModels/GameViewModel.cs
+++ ViewModels/GameViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,8 +14,8 @@ namespace TriviaAppClean.ViewModels
     {
         TriviaWebAPIProxy triviaService;
 
-        private List<string> answers;
-        public List<string> Answers
+        private ObservableCollection<string> answers;
+        public ObservableCollection<string> Answers
         {
             get { return answers; }
             set
@@ -23,10 +24,68 @@ namespace TriviaAppClean.ViewModels
                 OnPropertyChanged();
             }
         }
+        public ICommand AnswerCommand1 { get; set; }
+
+        public ICommand AnswerCommand2 { get; set; }
+
+        public ICommand AnswerCommand3 { get; set; }
+
+        public ICommand AnswerCommand4 { get; set; }
+        public ICommand NextQuestionCommand { get; set; }
+        private Color answer1Color;
+        public Color Answer1Color
+        {
+            get { return this.answer1Color; }
+            set
+            {
+                this.answer1Color = value;
+                OnPropertyChanged();
+            }
+        }
+        private Color answer2Color;
+        public Color Answer2Color
+        {
+            get { return this.answer2Color; }
+            set
+            {
+                this.answer2Color = value;
+                OnPropertyChanged();
+            }
+        }
+        private Color answer3Color;
+        public Color Answer3Color
+        {
+            get { return this.answer3Color; }
+            set
+            {
+                this.answer3Color = value;
+                OnPropertyChanged();
+            }
+        }
+        private Color answer4Color;
+        public Color Answer4Color
+        {
+            get { return this.answer4Color; }
+            set
+            {
+                this.answer4Color = value;
+                OnPropertyChanged();
+            }
+        }
+
         public GameViewModel(TriviaWebAPIProxy service) 
         { 
             this.triviaService = service;
             this.NextQuestionCommand = new Command(GetQuestion);
+            this.AnswerCommand1 = new Command(GetCorrectAnswer1);
+            this.AnswerCommand2 = new Command(GetCorrectAnswer2);
+            this.AnswerCommand3 = new Command(GetCorrectAnswer3);
+            this.AnswerCommand4 = new Command(GetCorrectAnswer4);
+            this.Answer1Color = new Color();
+            this.Answer2Color = new Color();
+            this.Answer3Color = new Color();
+            this.Answer4Color = new Color();
+            Answers = new ObservableCollection<string>();
             GetQuestion();
 
         }
@@ -61,14 +120,7 @@ namespace TriviaAppClean.ViewModels
             }
         }
 
-        public ICommand AnswerCommand1 { get; set; }
-
-        public ICommand AnswerCommand2 { get; set; }
-
-        public ICommand AnswerCommand3 { get; set; }
-
-        public ICommand AnswerCommand4 { get; set; }
-        public ICommand NextQuestionCommand { get; set; }
+       
         
 
 
@@ -76,45 +128,81 @@ namespace TriviaAppClean.ViewModels
         {
             Question = await triviaService.GetRandomQuestion();
             ScrambleAnswers();
+        
         }
         private void ScrambleAnswers()
         {
-            this.Answers = new List<string>();
+            this.Answers.Clear();
             this.Answers.Add(Question.CorrectAnswer);
             this.Answers.Add(Question.Bad1);
             this.Answers.Add(Question.Bad2);
             this.Answers.Add(Question.Bad3);
+            OnPropertyChanged("Answers");
             
 
         }
-        private void GetCorrectAnswer(string answer)
+
+        
+        private void GetCorrectAnswer1()
         {
+            string answer = Answers[0];
           if  (question != null)
             {
                 if (question.CorrectAnswer == answer)
                 {
                     score = score + 10;
-                    ButtonColor = Colors.Green;
+                    Answer1Color = Colors.Green;
                 }
-                else if(question.CorrectAnswer == AnswerCommand2.ToString())
-                {
-                    score = score + 10;
-                    ButtonColor = Colors.Green;
-                }
-                else if (question.CorrectAnswer == AnswerCommand3.ToString())
-                {
-                    score = score + 10;
-                    ButtonColor = Colors.Green;
-                }
-                else
-                {
-                    score = score + 10;
-                    ButtonColor = Colors.Green;
-                }
+
+                Answer1Color = Colors.Red;
 
             }
         }
+        private void GetCorrectAnswer2()
+        {
+            string answer = Answers[1];
+            if (question != null)
+            {
+                if (question.CorrectAnswer == answer)
+                {
+                    score = score + 10;
+                    Answer1Color = Colors.Green;
+                }
 
+                Answer1Color = Colors.Red;
+
+            }
+        }
+        private void GetCorrectAnswer3()
+        {
+            string answer = Answers[2];
+            if (question != null)
+            {
+                if (question.CorrectAnswer == answer)
+                {
+                    score = score + 10;
+                    Answer1Color = Colors.Green;
+                }
+
+                Answer1Color = Colors.Red;
+
+            }
+        }
+        private void GetCorrectAnswer4()
+        {
+            string answer = Answers[3];
+            if (question != null)
+            {
+                if (question.CorrectAnswer == answer)
+                {
+                    score = score + 10;
+                    Answer1Color = Colors.Green;
+                }
+
+                Answer1Color = Colors.Red;
+
+            }
+        }
 
 
 
